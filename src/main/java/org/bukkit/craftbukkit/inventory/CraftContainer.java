PumpkinKnightStudio/CraftBukkit

SPIGOT-7327, SPIGOT-7289, SPIGOT-6818, SPIGOT-6223, SPIGOT-6089, SPIGOT-6046, SPIGOT-5954, SPIGOT-5705: Complete the BungeeChat API

src/main/java/org/bukkit/craftbukkit/inventory/CraftContainer.java
@@ -1,5 +1,7 @@
 package org.bukkit.craftbukkit.inventory;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.world.IInventory;
 import net.minecraft.world.entity.player.EntityHuman;
 import net.minecraft.world.entity.player.PlayerInventory;
@@ -49,48 +51,84 @@ public class CraftContainer extends Container {
     }
 
     public CraftContainer(final Inventory inventory, final EntityHuman player, int id) {
-        this(new InventoryView() {
+        this(new ContainerInventoryView(inventory, player), player, id);
+    }
 
-            private final String originalTitle = (inventory instanceof CraftInventoryCustom) ? ((CraftInventoryCustom.MinecraftInventory) ((CraftInventory) inventory).getInventory()).getTitle() : inventory.getType().getDefaultTitle();
-            private String title = originalTitle;
+    private static final class ContainerInventoryView extends InventoryView {
+
+        private final BaseComponent originalTitle;
+        private BaseComponent title;
+
+        private final Inventory inventory;
+        private final EntityHuman player;
+
+        public ContainerInventoryView(Inventory inventory, EntityHuman player) {
+            this.inventory = inventory;
+            this.player = player;
+            this.originalTitle = (inventory instanceof CraftInventoryCustom) ? ((CraftInventoryCustom.MinecraftInventory) ((CraftInventory) inventory).getInventory()).getTitleComponent() : new TextComponent(inventory.getType().getDefaultTitle());
+            this.title = originalTitle.duplicate();
+        }
+
+        @Override
+        public Inventory getTopInventory() {
+            return inventory;
+        }
+
+        @Override
+        public Inventory getBottomInventory() {
+            return getPlayer().getInventory();
+        }
+
+        @Override
+        public HumanEntity getPlayer() {
+            return player.getBukkitEntity();
+        }
+
+        @Override
+        public InventoryType getType() {
+            return inventory.getType();
+        }
+
+        @Override
+        public String getTitle() {
+            return BaseComponent.toLegacyText(title);
+        }
+
+        @Override
+        public String getOriginalTitle() {
+            return BaseComponent.toLegacyText(originalTitle);
+        }
+
+        @Override
+        public void setTitle(String title) {
+            this.components.setTitle(TextComponent.fromLegacy(title));
+        }
+
+        private final CraftComponents components = new CraftComponents();
+
+        private final class CraftComponents implements InventoryView.Components {
 
             @Override
-            public Inventory getTopInventory() {
-                return inventory;
+            public BaseComponent getTitle() {
+                return title.duplicate();
             }
 
             @Override
-            public Inventory getBottomInventory() {
-                return getPlayer().getInventory();
+            public BaseComponent getOriginalTitle() {
+                return originalTitle.duplicate();
             }
 
             @Override
-            public HumanEntity getPlayer() {
-                return player.getBukkitEntity();
+            public void setTitle(BaseComponent title) {
+                CraftInventoryView.sendInventoryTitleChange(ContainerInventoryView.this, title);
+                ContainerInventoryView.this.title = title.duplicate();
             }
+        }
 
-            @Override
-            public InventoryType getType() {
-                return inventory.getType();
-            }
-
-            @Override
-            public String getTitle() {
-                return title;
-            }
-
-            @Override
-            public String getOriginalTitle() {
-                return originalTitle;
-            }
-
-            @Override
-            public void setTitle(String title) {
-                CraftInventoryView.sendInventoryTitleChange(this, title);
-                this.title = title;
-            }
-
-        }, player, id);
+        @Override
+        public Components components() {
+            return components;
+        }
     }
 
     @Override

src/main/java/org/bukkit/craftbukkit/scoreboard/CraftTeam.java
@@ -3,6 +3,8 @@ package org.bukkit.craftbukkit.scoreboard;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.world.scores.ScoreboardTeam;
 import net.minecraft.world.scores.ScoreboardTeamBase;
 import net.minecraft.world.scores.ScoreboardTeamBase.EnumNameTagVisibility;
@@ -30,47 +32,32 @@ final class CraftTeam extends CraftScoreboardComponent implements Team {
 
     @Override
     public String getDisplayName() {
-        checkState();
-
-        return CraftChatMessage.fromComponent(team.getDisplayName());
+        return BaseComponent.toLegacyText(components.getDisplayName());
     }
 
     @Override
     public void setDisplayName(String displayName) {
-        Preconditions.checkArgument(displayName != null, "Display name cannot be null");
-        checkState();
-
-        team.setDisplayName(CraftChatMessage.fromString(displayName)[0]); // SPIGOT-4112: not nullable
+        this.components.setDisplayName((displayName != null) ? TextComponent.fromLegacy(displayName) : null);
     }
 
     @Override
     public String getPrefix() {
-        checkState();
-
-        return CraftChatMessage.fromComponent(team.getPlayerPrefix());
+        return BaseComponent.toLegacyText(components.getPrefix());
     }
 
     @Override
     public void setPrefix(String prefix) {
-        Preconditions.checkArgument(prefix != null, "Prefix cannot be null");
-        checkState();
-
-        team.setPlayerPrefix(CraftChatMessage.fromStringOrNull(prefix));
+        this.components.setDisplayName((prefix != null) ? TextComponent.fromLegacy(prefix) : null);
     }
 
     @Override
     public String getSuffix() {
-        checkState();
-
-        return CraftChatMessage.fromComponent(team.getPlayerSuffix());
+        return BaseComponent.toLegacyText(components.getSuffix());
     }
 
     @Override
     public void setSuffix(String suffix) {
-        Preconditions.checkArgument(suffix != null, "Suffix cannot be null");
-        checkState();
-
-        team.setPlayerSuffix(CraftChatMessage.fromStringOrNull(suffix));
+        this.components.setDisplayName((suffix != null) ? TextComponent.fromLegacy(suffix) : null);
     }
 
     @Override
@@ -283,6 +270,58 @@ final class CraftTeam extends CraftScoreboardComponent implements Team {
         Preconditions.checkState(getScoreboard().board.getPlayerTeam(team.getName()) != null, "Unregistered scoreboard component");
 
         return getScoreboard();
+    }
+
+    private final CraftComponents components = new CraftComponents();
+
+    private final class CraftComponents implements Team.Components {
+
+        @Override
+        public BaseComponent getDisplayName() {
+            checkState();
+            return CraftChatMessage.toBungee(team.getDisplayName());
+        }
+
+        @Override
+        public void setDisplayName(BaseComponent displayName) {
+            Preconditions.checkArgument(displayName != null, "displayName cannot be null");
+
+            checkState();
+            team.setDisplayName(CraftChatMessage.fromBungee(displayName));
+        }
+
+        @Override
+        public BaseComponent getPrefix() {
+            checkState();
+            return CraftChatMessage.toBungee(team.getPlayerPrefix());
+        }
+
+        @Override
+        public void setPrefix(BaseComponent prefix) {
+            Preconditions.checkArgument(prefix != null, "prefix cannot be null");
+
+            checkState();
+            team.setDisplayName(CraftChatMessage.fromBungee(prefix));
+        }
+
+        @Override
+        public BaseComponent getSuffix() {
+            checkState();
+            return CraftChatMessage.toBungee(team.getPlayerSuffix());
+        }
+
+        @Override
+        public void setSuffix(BaseComponent suffix) {
+            Preconditions.checkArgument(suffix != null, "suffix cannot be null");
+
+            checkState();
+            team.setDisplayName(CraftChatMessage.fromBungee(suffix));
+        }
+    }
+
+    @Override
+    public Components components() {
+        return components;
     }
 
     @Override

CoolCLK/Hammer-Server

src/main/java/org/bukkit/craftbukkit/inventory/CraftContainer.java
@@ -1,5 +1,7 @@
 package org.bukkit.craftbukkit.inventory;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.world.IInventory;
 import net.minecraft.world.entity.player.EntityHuman;
 import net.minecraft.world.entity.player.PlayerInventory;
@@ -49,48 +51,84 @@ public class CraftContainer extends Container {
     }
 
     public CraftContainer(final Inventory inventory, final EntityHuman player, int id) {
-        this(new CraftAbstractInventoryView() {
+        this(new ContainerInventoryView(inventory, player), player, id);
+    }
 
-            private final String originalTitle = (inventory instanceof CraftInventoryCustom) ? ((CraftInventoryCustom.MinecraftInventory) ((CraftInventory) inventory).getInventory()).getTitle() : inventory.getType().getDefaultTitle();
-            private String title = originalTitle;
+    private static final class ContainerInventoryView extends CraftAbstractInventoryView {
+
+        private final BaseComponent originalTitle;
+        private BaseComponent title;
+
+        private final Inventory inventory;
+        private final EntityHuman player;
+
+        public ContainerInventoryView(Inventory inventory, EntityHuman player) {
+            this.inventory = inventory;
+            this.player = player;
+            this.originalTitle = (inventory instanceof CraftInventoryCustom) ? ((CraftInventoryCustom.MinecraftInventory) ((CraftInventory) inventory).getInventory()).getTitleComponent() : new TextComponent(inventory.getType().getDefaultTitle());
+            this.title = originalTitle.duplicate();
+        }
+
+        @Override
+        public Inventory getTopInventory() {
+            return inventory;
+        }
+
+        @Override
+        public Inventory getBottomInventory() {
+            return getPlayer().getInventory();
+        }
+
+        @Override
+        public HumanEntity getPlayer() {
+            return player.getBukkitEntity();
+        }
+
+        @Override
+        public InventoryType getType() {
+            return inventory.getType();
+        }
+
+        @Override
+        public String getTitle() {
+            return BaseComponent.toLegacyText(title);
+        }
+
+        @Override
+        public String getOriginalTitle() {
+            return BaseComponent.toLegacyText(originalTitle);
+        }
+
+        @Override
+        public void setTitle(String title) {
+            this.components.setTitle(TextComponent.fromLegacy(title));
+        }
+
+        private final CraftComponents components = new CraftComponents();
+
+        private final class CraftComponents implements InventoryView.Components {
 
             @Override
-            public Inventory getTopInventory() {
-                return inventory;
+            public BaseComponent getTitle() {
+                return title.duplicate();
             }
 
             @Override
-            public Inventory getBottomInventory() {
-                return getPlayer().getInventory();
+            public BaseComponent getOriginalTitle() {
+                return originalTitle.duplicate();
             }
 
             @Override
-            public HumanEntity getPlayer() {
-                return player.getBukkitEntity();
+            public void setTitle(BaseComponent title) {
+                CraftInventoryView.sendInventoryTitleChange(ContainerInventoryView.this, title);
+                ContainerInventoryView.this.title = title.duplicate();
             }
+        }
 
-            @Override
-            public InventoryType getType() {
-                return inventory.getType();
-            }
-
-            @Override
-            public String getTitle() {
-                return title;
-            }
-
-            @Override
-            public String getOriginalTitle() {
-                return originalTitle;
-            }
-
-            @Override
-            public void setTitle(String title) {
-                CraftInventoryView.sendInventoryTitleChange(this, title);
-                this.title = title;
-            }
-
-        }, player, id);
+        @Override
+        public Components components() {
+            return components;
+        }
     }
 
     @Override

src/main/java/org/bukkit/craftbukkit/scoreboard/CraftTeam.java
@@ -3,6 +3,8 @@ package org.bukkit.craftbukkit.scoreboard;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.world.scores.ScoreboardTeam;
 import net.minecraft.world.scores.ScoreboardTeamBase;
 import net.minecraft.world.scores.ScoreboardTeamBase.EnumNameTagVisibility;
@@ -30,47 +32,32 @@ final class CraftTeam extends CraftScoreboardComponent implements Team {
 
     @Override
     public String getDisplayName() {
-        checkState();
-
-        return CraftChatMessage.fromComponent(team.getDisplayName());
+        return BaseComponent.toLegacyText(components.getDisplayName());
     }
 
     @Override
     public void setDisplayName(String displayName) {
-        Preconditions.checkArgument(displayName != null, "Display name cannot be null");
-        checkState();
-
-        team.setDisplayName(CraftChatMessage.fromString(displayName)[0]); // SPIGOT-4112: not nullable
+        this.components.setDisplayName((displayName != null) ? TextComponent.fromLegacy(displayName) : null);
     }
 
     @Override
     public String getPrefix() {
-        checkState();
-
-        return CraftChatMessage.fromComponent(team.getPlayerPrefix());
+        return BaseComponent.toLegacyText(components.getPrefix());
     }
 
     @Override
     public void setPrefix(String prefix) {
-        Preconditions.checkArgument(prefix != null, "Prefix cannot be null");
-        checkState();
-
-        team.setPlayerPrefix(CraftChatMessage.fromStringOrNull(prefix));
+        this.components.setPrefix((prefix != null) ? TextComponent.fromLegacy(prefix) : null);
     }
 
     @Override
     public String getSuffix() {
-        checkState();
-
-        return CraftChatMessage.fromComponent(team.getPlayerSuffix());
+        return BaseComponent.toLegacyText(components.getSuffix());
     }
 
     @Override
     public void setSuffix(String suffix) {
-        Preconditions.checkArgument(suffix != null, "Suffix cannot be null");
-        checkState();
-
-        team.setPlayerSuffix(CraftChatMessage.fromStringOrNull(suffix));
+        this.components.setSuffix((suffix != null) ? TextComponent.fromLegacy(suffix) : null);
     }
 
     @Override
@@ -283,6 +270,58 @@ final class CraftTeam extends CraftScoreboardComponent implements Team {
         Preconditions.checkState(getScoreboard().board.getPlayerTeam(team.getName()) != null, "Unregistered scoreboard component");
 
         return getScoreboard();
+    }
+
+    private final CraftComponents components = new CraftComponents();
+
+    private final class CraftComponents implements Team.Components {
+
+        @Override
+        public BaseComponent getDisplayName() {
+            checkState();
+            return CraftChatMessage.toBungee(team.getDisplayName());
+        }
+
+        @Override
+        public void setDisplayName(BaseComponent displayName) {
+            Preconditions.checkArgument(displayName != null, "displayName cannot be null");
+
+            checkState();
+            team.setDisplayName(CraftChatMessage.fromBungee(displayName));
+        }
+
+        @Override
+        public BaseComponent getPrefix() {
+            checkState();
+            return CraftChatMessage.toBungee(team.getPlayerPrefix());
+        }
+
+        @Override
+        public void setPrefix(BaseComponent prefix) {
+            Preconditions.checkArgument(prefix != null, "prefix cannot be null");
+
+            checkState();
+            team.setPlayerPrefix(CraftChatMessage.fromBungee(prefix));
+        }
+
+        @Override
+        public BaseComponent getSuffix() {
+            checkState();
+            return CraftChatMessage.toBungee(team.getPlayerSuffix());
+        }
+
+        @Override
+        public void setSuffix(BaseComponent suffix) {
+            Preconditions.checkArgument(suffix != null, "suffix cannot be null");
+
+            checkState();
+            team.setPlayerSuffix(CraftChatMessage.fromBungee(suffix));
+        }
+    }
+
+    @Override
+    public Components components() {
+        return components;
     }
 
     @Override