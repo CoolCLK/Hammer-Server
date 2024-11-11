a8c8cdd51df32503ba44541d33ca3c579c0281f7
Doc <nachito94@msn.com>
2024-11-11 08:54:43 -0300

    Minor changes

--- src/main/java/org/bukkit/craftbukkit/inventory/components/consumable/effects/CraftConsumableEffect.java
+++ src/main/java/org/bukkit/craftbukkit/inventory/components/consumable/effects/CraftConsumableEffect.java
@@ -8,7 +8,7 @@ import org.bukkit.inventory.meta.components.consumable.effects.ConsumableEffect;
 
 public abstract class CraftConsumableEffect<T extends ConsumeEffect> implements ConsumableEffect {
 
-    public static <T extends CraftConsumableEffect> T minecraftToBukkitSpecific(ConsumeEffect effect) {
+    public static <T extends CraftConsumableEffect<?>> T minecraftToBukkitSpecific(ConsumeEffect effect) {
         if (effect instanceof TeleportRandomlyConsumeEffect nmsEffect) {
             return ((T) new CraftConsumableTeleportRandomly(nmsEffect));
         } else if (effect instanceof RemoveStatusEffectsConsumeEffect nmsEffect) {
@@ -23,7 +23,7 @@ public abstract class CraftConsumableEffect<T extends ConsumeEffect> implements 
         throw new IllegalStateException("Unexpected value: " + effect.getType());
     }
 
-    public static <T extends ConsumeEffect> T bukkitToMinecraftSpecific(CraftConsumableEffect effect) {
+    public static <T extends ConsumeEffect> T bukkitToMinecraftSpecific(CraftConsumableEffect<T> effect) {
         if (effect instanceof CraftConsumableTeleportRandomly bukkitEffect) {
             return (T) bukkitEffect.getHandle();
         } else if (effect instanceof CraftConsumableRemoveEffect bukkitEffect) {
@@ -38,8 +38,8 @@ public abstract class CraftConsumableEffect<T extends ConsumeEffect> implements 
         this.handle = consumeEffect;
     }
 
-    public CraftConsumableEffect(CraftConsumableEffect consumeEffect) {
-        this.handle = (T) consumeEffect.handle;
+    public CraftConsumableEffect(CraftConsumableEffect<T> consumeEffect) {
+        this.handle = consumeEffect.handle;
     }
 
     public CraftConsumableEffect(Map<String, Object> map) {
